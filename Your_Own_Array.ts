14fc569fd86298718d06fb26de4cb5ff57a09ebb
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:44:18 -0800

    Add files via upload

---
+++ Your_Own_Array.ts
@@ -0,0 +1,8 @@
+
+
+let transportation :string[]= ['motercycle','car','bus','taxi'];
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a ' + transportation[i])
+}
